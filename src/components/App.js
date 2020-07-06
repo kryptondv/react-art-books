68eadadedaf553f9e73ca39e85a474fd3e32296a
kryptondv <kryptondevpl@gmail.com>
2020-07-06 13:47:09 +0200

    add pages and setup react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AboutPage from '../pages/AboutPage';
+import CartPage from '../pages/CartPage';
+import ContactPage from '../pages/ContactPage';
+import ErrorPage from '../pages/ErrorPage';
+import HomePage from '../pages/HomePage';
+import ProductsPage from '../pages/ProductsPage';
+import SingleProductPage from '../pages/SingleProductPage';
 
 function App() {
   return (
-    <div >
-      app
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/o-nas" component={AboutPage} />
+        <Route path="/kontakt" component={ContactPage} />
+        <Route exact path="/ksiazki" component={ProductsPage} />
+        <Route path="/ksiazki/:slug" component={SingleProductPage} />
+        <Route path="/koszyk" component={CartPage} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </>
   );
 }
 
