4d8cc1a58c7894bf36e875e161b08d6c40e0733b
kryptondv <kryptondevpl@gmail.com>
2020-07-06 18:55:04 +0200

    test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,15 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
+      Home page
     </>
   )
 }
