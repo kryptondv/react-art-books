f54708437f53b429f67eef51970be7db9c841b90
kryptondv <kryptondevpl@gmail.com>
2020-07-06 12:57:22 +0200

    initial setup

---
+++ src/assets/data/data.js
@@ -0,0 +1,268 @@
+import book0 from '../img/products/book-0.png';
+import book1 from '../img/products/book-1.png';
+import book2 from '../img/products/book-2.png';
+import book3 from '../img/products/book-3.png';
+import book4 from '../img/products/book-4.png';
+import book5 from '../img/products/book-5.png';
+import book6 from '../img/products/book-6.png';
+import book7 from '../img/products/book-7.png';
+import book8 from '../img/products/book-8.png';
+import book9 from '../img/products/book-9.png';
+import book10 from '../img/products/book-10.png';
+import book11 from '../img/products/book-11.png';
+import book12 from '../img/products/book-12.png';
+import book13 from '../img/products/book-13.png';
+import book14 from '../img/products/book-14.png';
+import book15 from '../img/products/book-15.png';
+import book16 from '../img/products/book-16.png';
+
+export const items = [
+         {
+           sys: { id: 0 },
+           fields: {
+             author: 'Elizabeth Wilhide',
+             title: 'Design. The Whole Story',
+             price: 89,
+             category: 'Design',
+             description:
+               'An essential visual guide to the sophisticated attempts of manufacturers to woo us with high functionality, good looks – and that indefinable feeling of the future.',
+             featured: false,
+             image: { fields: { file: { url: book0 } } },
+           },
+         },
+         {
+           sys: { id: 1 },
+
+           fields: {
+             author: 'Oliver Elser, Philip Kurz',
+             title: 'SOS Brutalism. A Global Survey',
+             price: 269,
+             category: 'Architecture',
+             description:
+               'Widely disliked in their heyday and only recently beginning to be appreciated, brutalist buildings around the world are at risk of being lost—in many cases to demolition, and in some to insensitive reconstructions that would forever alter buildings’ appearance beyond recognition. SOS Brutalism is a distress signal, an attempt to galvanize public awareness of the architectural heritage that is at risk of being forever lost. The book, result of a major collaborative research undertaking by Deutsches Architektur Museum DAM and Wüstenrot Foundation, presents a global survey of brutalist architecture of the 1950s, ’60s, and ’70s, covering around 120 key buildings from the period from around the world, many of them little-known and in imminent danger of destruction. Case studies of hotspots such as the Macedonian capital Skopje or New Haven, Connecticut, and essays on the history and theory of brutalism round out this groundbreaking and lavishly illustrated book.',
+             featured: true,
+
+             image: { fields: { file: { url: book1 } } },
+           },
+         },
+         {
+           sys: { id: 2 },
+
+           fields: {
+             author: 'Harry Gruyaert',
+             title: 'Last Call',
+             price: 129,
+             category: 'Photography',
+             description:
+               'A new collection from the award-winning Magnum photographer. Alongside American photographers such as Saul Leiter, Joel Meyerowitz, Stephen Shore and William Eggleston, Harry Gruyaert became one of the few European pioneers to explore the creative possibilities of colour in the 1970s and 1980s. The previous decades had elevated black and white photography to the realms of art, relegating colour to use in advertising, the press and illustration. Gruyaert’s work suggested new territory for colour photography: an emotive, non-narrative and boldly graphic way of perceiving the world.',
+             featured: false,
+
+             image: { fields: { file: { url: book2 } } },
+           },
+         },
+         {
+           sys: { id: 3 },
+
+           fields: {
+             author: 'Dieter Buchhart, Eleanor Nairne, Lotte Johnson',
+             title: 'Basquiat. Boom for Real',
+             price: 165,
+             category: 'Art',
+             description:
+               'Basquiat first came to prominence when he collaborated with Al Diaz to spray-paint enigmatic statements under the pseudonym SAMO©. He went on to work on collages, Xerox art, postcards, performances, and music before establishing his reputation as one of the most important painters of his generation. Accompanying a major exhibition at the Barbican Art Gallery, this book opens with introductory essays from the curators, which place his practice in a wider art historical context and look at his career through the lens of performance. Six thematic chapters offer new research, with essays from poet Christian Campbell on SAMO©; curator Carlo McCormick on New York / New Wave; writer Glenn O’Brien on the downtown scene; academic Jordana Moore Saggese on Basquiat’s relationship to film and television; and music scholar Francesco Martinelli on Basquiat’s obsession with jazz. This insightful new survey also features extended captions, rare archival material, and extensive photography, demonstrating how Basquiat’s legacy remains more powerful and relevant than ever today.',
+             featured: true,
+
+             image: { fields: { file: { url: book3 } } },
+           },
+         },
+         {
+           sys: { id: 4 },
+
+           fields: {
+             author: 'Magdalena Droste',
+             title: 'Bauhaus',
+             price: 89,
+             category: 'Design',
+             description:
+               'In a fleeting 14-year period between two world wars, Germany’s Bauhaus school of art and design changed the face of modernity. With utopian ideals for the future, the school developed a pioneering fusion of fine art, craftsmanship, and technology which they applied across artistic media and practices, from film to theater, sculpture to ceramics.',
+             featured: false,
+
+             image: { fields: { file: { url: book4 } } },
+           },
+         },
+         {
+           sys: { id: 5 },
+
+           fields: {
+             author: 'Annie Leibovitz',
+             title: 'Annie Leibovitz at Work',
+             price: 175,
+             category: 'Photography',
+             description:
+               'Leibovitz addresses young photographers and readers interested in what photographers do, but any reader interested in contemporary history will be fascinated by her account of one of the richest bodies of work in the photographic canon. The subjects include photojournalism, studio work, photographing dancers and athletes, working with writers, and making the transition from shooting with film to working with digital cameras. Originally published in 2008, this revised and updated edition brings Leibovitz’s bestselling book back into print.',
+             featured: false,
+
+             image: { fields: { file: { url: book5 } } },
+           },
+         },
+
+         {
+           sys: { id: 6 },
+
+           fields: {
+             author: 'Roger Keyes, Timothy Clark',
+             title: 'Hokusai. Beyond the Great Wave',
+             price: 159,
+             category: 'Art',
+             description:
+               'Hokusai created sublime works during the last thirty years of his life, right up to his death at the age of ninety. Publications have hitherto presented his long career as a chronological sequence. This book takes a fresh approach based on innovative scholarship: thematic groupings of works are related to the major spiritual and artistic quests of Hokusai’s life.',
+             featured: false,
+
+             image: { fields: { file: { url: book6 } } },
+           },
+         },
+         {
+           sys: { id: 7 },
+
+           fields: {
+             author: 'Cohen Jean-Louis',
+             title: 'Le Corbusier. Le Grand',
+             price: 199,
+             category: 'Architecture',
+             description:
+               'Drawing on an array of archival material, including sketches, photographs, and correspondences, Le Corbusier Le Grand depicts in roughly chronological order not only the vast and varied output of Le Corbusier, but also the major events, people, and forces that shaped the life of an artist who continues to fascinate those in and outside the architectural world.',
+             featured: false,
+
+             image: { fields: { file: { url: book7 } } },
+           },
+         },
+         {
+           sys: { id: 8 },
+
+           fields: {
+             author: 'Masanao Amano',
+             title: '100 Manga Artists',
+             price: 75,
+             category: 'Art',
+             description:
+               'Since the original TASCHEN edition of Manga Design, Japan’s comic phenomenon has produced yet more captivating characters and a whole host of hot new talents. This revised and updated edition delivers the lowdown on the latest and the greatest makers and shapers of the manga scene.',
+             featured: true,
+
+             image: { fields: { file: { url: book8 } } },
+           },
+         },
+         {
+           sys: { id: 9 },
+
+           fields: {
+             author: 'Maryam Omidi',
+             title: 'Holidays in Soviet Sanatoriums',
+             price: 89,
+             category: 'Architecture',
+             description:
+               'This book is the first to offer a comprehensive collection of photographs and text on Soviet-era sanatoriums from Armenia to Uzbekistan. All the photographs are specially commissioned for the book, taken by a team of young photographers specialising in the post-Soviet territories.',
+             featured: true,
+
+             image: { fields: { file: { url: book9 } } },
+           },
+         },
+         {
+           sys: { id: 10 },
+
+           fields: {
+             author: 'Sandra Rendgen',
+             title: 'Information Graphics',
+             price: 239,
+             category: 'Design',
+             description:
+               'Our everyday lives are filled with a massive flow of information that we must interpret in order to understand the world we live in. Considering this complex variety of data floating around us, sometimes the best — or even only — way to communicate is visually. This unique book presents a fascinating perspective on the subject, highlighting the work of the masters of the profession who have created a number of breakthroughs that have changed the way we communicate. Information Graphics has been conceived and designed not just for graphics professionals, but for anyone interested in the history and practice of communicating visually.',
+             featured: false,
+
+             image: { fields: { file: { url: book10 } } },
+           },
+         },
+         {
+           sys: { id: 11 },
+
+           fields: {
+             author: 'Francesco Spampinato',
+             title: 'Art Record Covers',
+             price: 239,
+             category: 'Art',
+             description:
+               'Since the dawn of modernism, visual and music production have had a particularly intimate relationship. From Luigi Russolo’s 1913 Futurist manifesto L’Arte dei Rumori (The Art of Noise) to Marcel Duchamp’s 1925 double-sided discs Rotoreliefs, the 20th century saw ever more fertile exchange between sounds and shapes, marks and melodies, and different fields of composition and performance.',
+             featured: false,
+             image: { fields: { file: { url: book11 } } },
+           },
+         },
+         {
+           sys: { id: 12 },
+
+           fields: {
+             author: 'Caravaggio. Complete Works',
+             title: 'Sebastian Schütze',
+             price: 239,
+             category: 'Art',
+             description:
+               'Caravaggio, or more accurately Michelangelo Merisi da Caravaggio (1571–1610), was always a name to be reckoned with. Notorious bad boy of Italian painting, the artist was at once celebrated and controversial: Violent in temper, precise in technique, a creative master, and a man on the run.',
+             featured: false,
+             image: { fields: { file: { url: book12 } } },
+           },
+         },
+         {
+           sys: { id: 13 },
+
+           fields: {
+             author: 'Alston W. Purvis, Cees W. de Jong, Jan Tholenaar',
+             title: 'Type. A Visual History of Typefaces & Graphic Styles',
+             price: 235,
+             category: 'Design',
+             description:
+               'This comprehensive collection offers a thorough overview of typeface design from 1628 to the mid-20th century. Derived from a distinguished Dutch collection, a series of exquisitely designed catalogues traces the evolution of the printed letter via specimens in roman, italic, bold, semibold, narrow, and broad fonts. Borders, ornaments, initial letters, and decorations are also included, along with lithographic examples, letters by sign writers, inscription carvers, and calligraphers.',
+             featured: false,
+             image: { fields: { file: { url: book13 } } },
+           },
+         },
+         {
+           sys: { id: 14 },
+
+           fields: {
+             author: 'Philip Jodidio',
+             title: 'Ando. Complete Works 1975–Today.',
+             price: 239,
+             category: 'Architecture',
+             description:
+               'Discover the completely unique aesthetic of Tadao Ando, the only architect ever to have won the discipline’s four most prestigious prizes: the Pritzker, Carlsberg, Praemium Imperiale, and Kyoto Prize. Philippe Starck defines him as a “mystic in a country which is no longer mystic.” Philip Drew calls his buildings “land art” as they “struggle to emerge from the earth.” His designs have been described as haiku crafted from concrete, water, light, and space. But to Ando, true architecture is not expressed in metaphysics or beauty, but rather through space that embodies physical wisdom.',
+             featured: false,
+             image: { fields: { file: { url: book14 } } },
+           },
+         },
+         {
+           sys: { id: 15 },
+
+           fields: {
+             author: 'Sebastiao Salgado',
+             title: 'Genesis',
+             price: 239,
+             category: 'Photography',
+             description:
+               'On a very fortuitous day in 1970, 26-year-old Sebastião Salgado held a camera for the first time. When he looked through the viewfinder, he experienced a revelation: suddenly life made sense. From that day onward—though it took years of hard work before he had the experience to earn his living as a photographer—the camera became his tool for interacting with the world. Salgado, who “always preferred the chiaroscuro palette of black-and-white images,” shot very little color in his early career before giving it up completely.',
+             featured: false,
+             image: { fields: { file: { url: book15 } } },
+           },
+         },
+         {
+           sys: { id: 16 },
+
+           fields: {
+             author: 'Stephen McLaren',
+             title: 'Magnum Streetwise',
+             price: 384,
+             category: 'Photography',
+             description:
+               'Magnum Streetwise is an unmissable tour through the photographs and practices that have helped define what street photography is and can be. Magnum photographers such as Henri Cartier-Bresson pioneered ‘modern’ concepts of street photography before the term was even coined – but their influence is far from historic. A rich seam of street photography runs through the heart of Magnum to this day, both in the work of recognized masters of street photography – such as Erwitt, Parr, Gilden and Kalvar – and of those who might not even consider themselves street photographers; a continued influence that has not gone unnoticed among the current generation of budding street photographers and fans.',
+             featured: false,
+             image: { fields: { file: { url: book16 } } },
+           },
+         },
+       ];
