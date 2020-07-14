d585d73193b862ed3cd695432b576cf768fdc2c7
kryptondv <kryptondevpl@gmail.com>
2020-07-14 14:26:30 +0200

    add cart columns

--- src/components/CartPage/CartColumns.js
+++ src/components/CartPage/CartColumns.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const CartColums = () => {
   return (
-    <div>
-      cart columns
+    <div className="cart-columns">
+      <div className="cart-columns__item">Tytuł</div>
+      <div className="cart-columns__item">Autor</div>
+      <div className="cart-columns__item">Cena</div>
+      <div className="cart-columns__item">Ilość</div>
+      <div className="cart-columns__item">Usuń</div>
+      <div className="cart-columns__item">Suma</div>
     </div>
   );
 }
